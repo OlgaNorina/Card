10ce27a814ef9e60551422523dbae3e7a9d17c49
Olga <norinaoa@mail.ru>
2020-05-03 18:13:59 +0500

    -  chmod +x gradlew added

--- src/test/java/Test.java
+++ src/test/java/Test.java
@@ -9,11 +9,11 @@ class CallbackTest {
     @Test
     void shouldSubmitRequest() {
         open("http://localhost:9999");
-        SelenideElement form = $("[data-test-id=callback-form]");
-        form.$("[data-test-id=name] input").setValue("Василий");
+        SelenideElement form = $(".form");
+        form.$("[data-test-id=name] input").setValue("Васильев Василий");
         form.$("[data-test-id=phone] input").setValue("+79270000000");
         form.$("[data-test-id=agreement]").click();
-        form.$("[data-test-id=submit]").click();
-        $(".alert-success").shouldHave(exactText("Ваша заявка успешно отправлена!"));
+        form.$(".button").click();
+        $(".Success_successBlock__2L3Cw").shouldHave(exactText("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время."));
     }
 }
